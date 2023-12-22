e2329f72c620d34d940f1e13e81929298cec2d66
anonymous <itisme1412@gmail.com>
2023-12-22 15:01:57 +0530

    view binding added

--- app/src/main/java/com/example/myapplication56789/MainActivity.kt
+++ app/src/main/java/com/example/myapplication56789/MainActivity.kt
@@ -5,19 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import com.example.myapplication56789.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     @Inject
     lateinit var teststing : String
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(binding.root)
+        binding.tv.setText(teststing)
         Log.d("main", "this is string 32134567 : ${teststing} ")
     }
 }
